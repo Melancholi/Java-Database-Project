faa49582f1f945e75e68d5b21197724c080473d2
Youry Nelson <yourymorgannelson@gmail.com>
2023-11-13 01:11:24 -0500

    Added basic functions and procedures to packages

--- SuperStorePackage.sql
+++ SuperStorePackage.sql
@@ -1,70 +1,335 @@
-DROP PACKAGE SuperStorePackage;
-CREATE OR REPLACE PACKAGE ProductsPackage AS
-    
+CREATE OR REPLACE PACKAGE SuperStorePackage AS
+
     --Products table
-    PROCEDURE AddProduct(productName VARCHAR2,price NUMBER,store VARCHAR2,productCategory VARCHAR2);
-    FUNCTION getProduct(productName VARCHAR2)
-    RETURN productObj;
-    PROCEDURE updateProduct(productName VARCHAR2,price NUMBER,store VARCHAR2,category VARCHAR2);
-    PROCEDURE deleteProduct(productName VARCHAR2);
-    TYPE productObj IS OBJECT(
-        productName VARCHAR2(75),
-        price NUMBER(5,2),
-        store  VARCHAR2(100),
-        category VARCHAR2(100)
-        );
+    PROCEDURE AddProduct(product ProductObj);
+    PROCEDURE UpdateProduct(productNameToChange VARCHAR2,price NUMBER,store VARCHAR2,category VARCHAR2);
+    PROCEDURE DeleteProduct(productName VARCHAR2);
+
     
     -- Warehouse table
-    PROCEDURE AddWarehouse(warehouseName VARCHAR2, warehouseAddress VARCHAR2);
-    FUNCTION getWarehouse(warehouseName VARCHAR2)
-    RETURN warehouseObj;
-    PROCEDURE updateWarehouse(warehouseName VARCHAR2, warehouseAddress VARCHAR2);
-    PROCEDURE deleteWarehouse(warehouseName VARCHAR2);
-    TYPE warehouseObj IS OBJECT(
-      warehouseName VARCHAR2(100),
-      warehouseAddress VARCHAR2(150)
-    );
+    PROCEDURE AddWarehouse(warehouse warehouseObj);
+    PROCEDURE UpdateWarehouse(warehouseName VARCHAR2, warehouseAddress VARCHAR2);
+    PROCEDURE DeleteWarehouse(warehouseName VARCHAR2);
+
 
     -- Orders table
-    PROCEDURE AddOrder(customerID NUMBER, productName VARCHAR2, quantity NUMBER, orderDate DATE);
-    FUNCTION getOrder(orderID NUMBER)
-    RETURN orderObj;
-    PROCEDURE updateOrder(orderID NUMBER, customerID NUMBER, productName VARCHAR2, quantity NUMBER, orderDate DATE);
-    PROCEDURE deleteOrder(orderID NUMBER);
-    TYPE orderObj IS OBJECT(
-      orderID NUMBER(4),
-      customerID NUMBER(4),
-      productName VARCHAR2(75),
-      quantity NUMBER(3),
-      orderDate DATE
-    );
+    PROCEDURE AddOrder(orderO orderObj);
+    PROCEDURE UpdateOrder(orderID NUMBER, customerID NUMBER, productName VARCHAR2, quantity NUMBER, orderDate DATE);
+    PROCEDURE DeleteOrder(orderID NUMBER);
+
     
     -- Product Review Table
-    PROCEDURE AddOrderReview(orderID NUMBER, review VARCHAR2, reviewDescription VARCHAR2, reviewFlag VARCHAR2);
-    FUNCTION GetOrderReview(orderID NUMBER)
-    RETURN orderReviewObj;
-    PROCEDURE UpdateOrderReview(orderID NUMBER, review VARCHAR2, reviewDescription VARCHAR2, reviewFlag VARCHAR2);
-    PROCEDURE DeleteOrderReview(orderID NUMBER);
-    TYPE orderReviewObj IS OBJECT (
-        orderID NUMBER(4),
-        review VARCHAR2(30),
-        reviewDescription VARCHAR2(350),
-        reviewFlag VARCHAR2(100)
-    );
+    PROCEDURE AddProductReview(reviewO productReviewObj);
+    PROCEDURE UpdateProductReview(orderID NUMBER, review VARCHAR2, reviewDescription VARCHAR2, reviewFlag NUMBER);
+    PROCEDURE DeleteProductReview(orderID NUMBER);
+
     
     -- Customers table
-    PROCEDURE AddCustomer(customerEmail VARCHAR2, firstName VARCHAR2, lastName VARCHAR2, customerAddress VARCHAR2);
-    FUNCTION getCustomer(customerID NUMBER)
+    PROCEDURE AddCustomer(customer customerObj);
+    PROCEDURE UpdateCustomer(customerID NUMBER, customerEmail VARCHAR2, firstName VARCHAR2, lastName VARCHAR2, customerAddress VARCHAR2);
+    PROCEDURE DeleteCustomer(customerID NUMBER);  
+
+    
+    -- Warehouse Inventory
+    PROCEDURE AddWarehouseInventory(inventory warehouseInventoryObj);
+    PROCEDURE UpdateWarehouseInventory(productName VARCHAR2, warehouseName VARCHAR2, quantity NUMBER);
+    PROCEDURE DeleteWarehouseInventory(productName VARCHAR2, warehouseName VARCHAR2);
+
+    FUNCTION GetCustomer(customerID NUMBER)
     RETURN customerObj;
-    PROCEDURE updateCustomer(customerID NUMBER, customerEmail VARCHAR2, firstName VARCHAR2, lastName VARCHAR2, customerAddress VARCHAR2);
-    PROCEDURE deleteCustomer(customerID NUMBER);
-    TYPE customerObj IS OBJECT(
-      customerID NUMBER(4),
-      customerEmail VARCHAR2(200),
-      firstName VARCHAR2(100),
-      lastName VARCHAR2(100),
-      customerAddress VARCHAR2(150)
-    );    
-        
-END ProductsPackage;
+--    FUNCTION GetProductReview(orderID NUMBER)
+--    RETURN ProductReviewObj;
+--    FUNCTION GetOrder(orderID NUMBER)
+--    RETURN orderObj;
+--    FUNCTION GetWarehouse(warehouseName VARCHAR2)
+--    RETURN warehouseObj;
+--    FUNCTION GetProduct(productName VARCHAR2)
+--    RETURN productObj;
+--    FUNCTION GetWarehouseInventory(productName VARCHAR2, warehouseName VARCHAR2)
+--    RETURN warehouseInventoryObj;
+    
+    
+    --EXCEPTIONS
+    
+    
+    --TRIGGERS
+    
+END SuperStorePackage;
+/
+CREATE OR REPLACE PACKAGE BODY SuperStorePackage AS
+    --Functions for Products Table
+    PROCEDURE AddProduct
+    (product IN productObj)
+    IS
+    BEGIN
+        INSERT INTO Products_Details(Product_name,price,store,product_category)
+        VALUES(product.product_name,product.price,product.store,product.product_category);
+        COMMIT;
+    END AddProduct;
+    
+
+    PROCEDURE UpdateProduct(productNameToChange VARCHAR2,price NUMBER,store VARCHAR2,category VARCHAR2)
+    AS
+    BEGIN
+        UPDATE Products_Details
+        SET
+        price=price, store=store, product_category=category
+        WHERE product_name = productNameToChange;
+        COMMIT;
+    END updateProduct;
+    
+    PROCEDURE DeleteProduct(productName VARCHAR2)
+    AS
+    BEGIN
+        DELETE Products_Details
+        WHERE product_name=productName;
+        COMMIT;
+    END deleteProduct;
+    
+    -- Procedure to Add Warehouse
+    PROCEDURE AddWarehouse(warehouse IN warehouseObj)
+    AS
+    BEGIN
+        INSERT INTO Warehouse_Details(warehouse_name, warehouse_address)
+        VALUES(warehouse.warehouse_name,warehouse.warehouse_address);
+        COMMIT;
+    END AddWarehouse;
+    
+
+    -- Procedure to Update Warehouse
+    PROCEDURE UpdateWarehouse(warehouseName VARCHAR2, warehouseAddress VARCHAR2)
+    AS
+    BEGIN
+        UPDATE Warehouse_Details
+        SET warehouse_address = warehouseAddress
+        WHERE warehouse_name = warehouseName;
+        COMMIT;
+    END updateWarehouse;
+    
+    -- Procedure to Delete Warehouse
+    PROCEDURE DeleteWarehouse(warehouseName VARCHAR2)
+    AS
+    BEGIN
+        DELETE FROM Warehouse_Details
+        WHERE warehouse_name = warehouseName;
+        COMMIT;
+    END deleteWarehouse;
+    
+    -- Procedure to Add Order
+    PROCEDURE AddOrder(orderO IN orderObj)
+    AS
+    BEGIN
+        INSERT INTO Orders_Details(customerID, product_name, quantity, order_date)
+        VALUES (orderO.customerID,orderO.product_name,orderO.quantity,orderO.order_date);
+        COMMIT;
+    END AddOrder;
+    
+   
+    -- Procedure to Update Order
+    PROCEDURE UpdateOrder(orderID NUMBER, customerID NUMBER, productName VARCHAR2, quantity NUMBER, orderDate DATE)
+    AS
+    BEGIN
+        UPDATE Orders_Details
+        SET customerID = customerID, product_name = productName, quantity = quantity, order_date = orderDate
+        WHERE orderid = orderID;
+        COMMIT;
+    END updateOrder;
+    
+    -- Procedure to Delete Order
+    PROCEDURE DeleteOrder(orderID NUMBER)
+    AS
+    BEGIN
+        DELETE FROM Orders_Details
+        WHERE orderid = orderID;
+        COMMIT;
+    END deleteOrder;
+    
+    
+     -- Product Review Table
+    PROCEDURE AddProductReview(reviewO IN productReviewObj)
+    AS
+    BEGIN
+        INSERT INTO Product_Review(orderid, review, review_description, review_flag)
+        VALUES(reviewO.orderid,reviewO.review,reviewO.review_description,reviewO.review_flag);
+        COMMIT;
+    END AddProductReview;
+
+    PROCEDURE UpdateProductReview(orderID NUMBER, review VARCHAR2, reviewDescription VARCHAR2, reviewFlag NUMBER)
+    AS
+    BEGIN
+        UPDATE Product_Review
+        SET review = review, review_description = reviewDescription, review_flag = reviewFlag
+        WHERE orderid = orderID;
+        COMMIT;
+    END UpdateProductReview;
+
+    PROCEDURE DeleteProductReview(orderID NUMBER)
+    AS
+    BEGIN
+        DELETE FROM Product_Review
+        WHERE orderid = orderID;
+        COMMIT;
+    END DeleteProductReview;
+    
+    
+    -- Customers table
+    PROCEDURE AddCustomer(customer IN customerObj)
+    AS
+    BEGIN
+        INSERT INTO Customers_Details(customer_email, first_name, last_name, customer_address)
+        VALUES(customer.customer_email,customer.first_name,customer.last_name,customer.customer_address);
+        COMMIT;
+    END AddCustomer;
+
+    PROCEDURE UpdateCustomer(customerID NUMBER, customerEmail VARCHAR2, firstName VARCHAR2, lastName VARCHAR2, customerAddress VARCHAR2)
+    AS
+    BEGIN
+        UPDATE Customers_Details
+        SET customer_email = customerEmail, first_name = firstName, last_name = lastName, customer_address = customerAddress
+        WHERE customerID = customerID;
+        COMMIT;
+    END UpdateCustomer;
+
+    PROCEDURE DeleteCustomer(customerID NUMBER)
+    AS
+    BEGIN
+        DELETE FROM Customers_Details
+        WHERE customerID = customerID;
+        COMMIT;
+    END DeleteCustomer;
+
+    -- Warehouse Inventory
+    PROCEDURE AddWarehouseInventory(inventory IN warehouseInventoryObj)
+    AS
+    BEGIN
+        INSERT INTO Warehouse_Inventory(product_name, warehouse_name, quantity)
+        VALUES(inventory.product_name,inventory.warehouse_name,inventory.quantity);
+        COMMIT;
+    END AddWarehouseInventory;
+
+    PROCEDURE UpdateWarehouseInventory(productName VARCHAR2, warehouseName VARCHAR2, quantity NUMBER)
+    AS
+    BEGIN
+        UPDATE Warehouse_Inventory
+        SET quantity = quantity
+        WHERE product_name = productName AND warehouse_name = warehouseName;
+        COMMIT;
+    END UpdateWarehouseInventory;
+
+    PROCEDURE DeleteWarehouseInventory(productName VARCHAR2, warehouseName VARCHAR2)
+    AS
+    BEGIN
+        DELETE FROM Warehouse_Inventory
+        WHERE product_name = productName AND warehouse_name = warehouseName;
+        COMMIT;
+    END DeleteWarehouseInventory;
+
+ --GETTERS FOR THE TABLES
+ --Idea of how to do my getter
+    FUNCTION GetCustomer(customerID NUMBER)
+    RETURN customerObj
+    AS
+        vCustomer customerObj;
+    BEGIN
+        SELECT customerID, customer_email, first_name, last_name, customer_address
+        INTO vCustomer.customerID,vCustomer.customer_email,vCustomer.first_name,vCustomer.last_name,vCustomer.customer_address
+        FROM Customers_Details
+        WHERE customerID = customerID;
+
+        RETURN vCustomer;
+
+        EXCEPTION 
+            WHEN NO_DATA_FOUND THEN
+                dbms_output.put_line('No data found for customer ID given');
+    END GetCustomer;
+--
+--    FUNCTION GetProductReview(orderID NUMBER)
+--    RETURN ProductReviewObj
+--    AS
+--        vProductReview ProductReviewObj;
+--    BEGIN
+--        SELECT orderid, review, review_description, review_flag
+--        INTO vProductReview
+--        FROM Product_Review
+--        WHERE orderid = orderID;
+--
+--        RETURN vProductReview;
+--
+--        EXCEPTION 
+--            WHEN NO_DATA_FOUND THEN
+--                dbms_output.put_line('No data found for order ID given');
+--    END GetProductReview;
+--
+--  --Function to Get Order
+--    FUNCTION getOrder(orderID NUMBER)
+--    RETURN orderObj
+--    AS
+--        vOrder orderObj;
+--    BEGIN
+--        SELECT orderid, customerID, product_name, quantity, order_date
+--        INTO vOrder
+--        FROM Orders_Details
+--        WHERE orderid = orderID;
+--    
+--        RETURN vOrder;
+--    
+--        EXCEPTION 
+--            WHEN NO_DATA_FOUND THEN
+--                dbms_output.put_line('No data found for order ID given');
+--    END getOrder;
+--
+--    -- Function to Get Warehouse
+--    FUNCTION GetWarehouse(warehouseName VARCHAR2)
+--    RETURN warehouseObj
+--    IS
+--        vWarehouse warehouseObj;
+--    BEGIN
+--        SELECT warehouse_name,warehouse_address
+--        INTO vWarehouse
+--        FROM Warehouse_Details
+--        WHERE warehouse_name = warehouseName;
+--    
+--        RETURN vWarehouse;
+--    
+--        EXCEPTION 
+--            WHEN NO_DATA_FOUND THEN
+--                dbms_output.put_line('No data found for warehouse name given');
+--    END getWarehouse;
+--
+--    FUNCTION GetProduct(productName VARCHAR2)
+--    RETURN productObj
+--    AS
+--        vProduct productObj;
+--    BEGIN
+--        SELECT product_name,price,store,product_category
+--        INTO vProduct
+--        FROM Products_details
+--        WHERE product_name=productName;
+--        
+--        RETURN vProduct;
+--    
+--        EXCEPTION 
+--            WHEN NO_DATA_FOUND THEN
+--                dbms_output.put_line('No data found for product name given');
+--    END getProduct;
+--
+--    FUNCTION GetWarehouseInventory(productName VARCHAR2, warehouseName VARCHAR2)
+--    RETURN warehouseInventoryObj
+--    AS
+--        vWarehouseInventory warehouseInventoryObj;
+--    BEGIN
+--        SELECT product_name, warehouse_name, quantity
+--        INTO vWarehouseInventory
+--        FROM warehouse_inventory
+--        WHERE product_name = productName AND warehouse_name = warehouseName;
+--
+--        RETURN vWarehouseInventory;
+--
+--        EXCEPTION 
+--            WHEN NO_DATA_FOUND THEN
+--                dbms_output.put_line('No data found for product in the warehouse');
+--    END GetWarehouseInventory;
+ 
+END SuperStorePackage;
+/
 
--- TablesProject.sql
+++ TablesProject.sql
@@ -1,4 +1,7 @@
-DROP TABLE Warehouse_Product;
+--Add link to location from warehouse details
+--Add link to location from customer details
+
+DROP TABLE Warehouse_Inventory;
 DROP TABLE Product_Review;
 DROP TABLE Warehouse_Details;
 DROP TABLE Orders_Details;
@@ -12,7 +15,7 @@ CREATE TABLE Warehouse_details(
     );
 CREATE TABLE Customers_details(
     --So it updates id every time I make a new row
-    customerID NUMBER(4) GENERATED ALWAYS AS IDENTITY PRIMARY KEY ,
+    customerID NUMBER(4) GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     customer_email VARCHAR2(200),
     first_name VARCHAR2(100),
     last_name VARCHAR2(100),
@@ -42,15 +45,15 @@ CREATE TABLE Product_review(
     orderid    NUMBER(4),
     review      NUMBER(1) CHECK (review>1),
     review_description VARCHAR2(350),
-    review_flag VARCHAR2(100),
+    review_flag NUMBER(3) CHECK(review_flag>=0),
     CONSTRAINT review_order_fk
     FOREIGN KEY (orderid)
     REFERENCES Orders_details(orderid)
     );
-CREATE TABLE Warehouse_Product (
+CREATE TABLE Warehouse_Inventory(
     product_name    VARCHAR2(75),
     warehouse_name  VARCHAR2(100),
-    quantity    NUMBER(4) CHECK (quantity>0),
+    quantity    NUMBER(6) CHECK (quantity>0),
     CONSTRAINT product_warehouse_fk
     FOREIGN KEY (product_name)
     REFERENCES Products_Details(product_name),
@@ -58,4 +61,52 @@ CREATE TABLE Warehouse_Product (
     FOREIGN KEY (warehouse_name)
     REFERENCES Warehouse_details(warehouse_name)
     );
-    
+CREATE TABLE Location(
+    Country VARCHAR2(125) PRIMARY KEY,
+    City VARCHAR2(100)
+    );
+COMMIT;
+/
+
+CREATE OR REPLACE TYPE productObj IS OBJECT(
+        product_name VARCHAR2(75),
+        price NUMBER(5,2),
+        store  VARCHAR2(100),
+        product_category VARCHAR2(100)
+        );
+/
+CREATE OR REPLACE TYPE warehouseObj IS OBJECT(
+      warehouse_name VARCHAR2(100),
+      warehouse_address VARCHAR2(150)
+    );
+/
+CREATE OR REPLACE TYPE orderObj IS OBJECT(
+      orderid NUMBER(4),
+      customerID NUMBER(4),
+      product_name VARCHAR2(75),
+      quantity NUMBER(3),
+      order_date DATE
+    );
+
+/
+CREATE OR REPLACE TYPE productReviewObj IS OBJECT (
+        orderid NUMBER(4),
+        review VARCHAR2(30),
+        review_description VARCHAR2(350),
+        review_flag NUMBER(3)
+    );
+/
+CREATE OR REPLACE TYPE customerObj IS OBJECT(
+      customerID NUMBER(4),
+      customer_email VARCHAR2(200),
+      first_name VARCHAR2(100),
+      last_name VARCHAR2(100),
+      customer_address VARCHAR2(150)
+    );  
+/
+CREATE OR REPLACE TYPE warehouseInventoryObj IS OBJECT (
+        product_name VARCHAR2(75),
+        warehouse_name VARCHAR2(100),
+        quantity NUMBER(6)
+    );
+/
