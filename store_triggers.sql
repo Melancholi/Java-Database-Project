266e87ff082dda91005066e6fc0e7f41ec5a7c91
Youry Nelson <yourymorgannelson@gmail.com>
2023-11-20 14:31:42 -0500

    Added log audit and triggers for the logs

--- store_triggers.sql
+++ store_triggers.sql
@@ -58,3 +58,259 @@ BEGIN
         RAISE not_enough_items;
     END IF;
 END;
+/
+--TABLE AUDIT LOG TRIGGERS
+CREATE OR REPLACE TRIGGER location_audit
+AFTER INSERT OR UPDATE OR DELETE
+ON location_details
+FOR EACH ROW
+DECLARE
+    operation_type location_details.operation_type%TYPE;
+BEGIN
+    IF INSERTING THEN
+        operation_type:= 'INSERT';
+    END IF;
+    IF UPDATING THEN
+        operation_type:= 'UPDATE';
+    END IF;
+    IF DELETING THEN
+        operation_type:= 'DELETE';
+    END IF;
+    INSERT INTO location_log(AddressID,operation_type,oldAddress,newAddress,oldCountry,newCountry,oldCity,newCity)
+    VALUES(
+        :OLD.AddressID,
+        operation_type,
+        :OLD.address,
+        :NEW.address,
+        :OLD.country,
+        :NEW.country,
+        :OLD.city,
+        :NEW.city
+    );
+END;
+/
+CREATE OR REPLACE TRIGGER products_details_audit
+AFTER INSERT OR UPDATE OR DELETE
+ON products_details
+FOR EACH ROW
+DECLARE
+    operation_type VARCHAR2(10);
+BEGIN
+    IF INSERTING THEN
+        operation_type := 'INSERT';
+    ELSIF UPDATING THEN
+        operation_type := 'UPDATE';
+    ELSIF DELETING THEN
+        operation_type := 'DELETE';
+    END IF;
+
+    INSERT INTO products_details_log (
+        product_name,
+        operation_type,
+        old_price,
+        new_price,
+        old_store,
+        new_store,
+        old_product_category,
+        new_product_category
+    )
+    VALUES (
+        :OLD.product_name,
+        operation_type,
+        :OLD.price,
+        :NEW.price,
+        :OLD.store,
+        :NEW.store,
+        :OLD.product_category,
+        :NEW.product_category
+    );
+END;
+/
+CREATE OR REPLACE TRIGGER customers_details_audit
+AFTER INSERT OR UPDATE OR DELETE
+ON customers_details
+FOR EACH ROW
+DECLARE
+    operation_type VARCHAR2(10);
+BEGIN
+    IF INSERTING THEN
+        operation_type := 'INSERT';
+    ELSIF UPDATING THEN
+        operation_type := 'UPDATE';
+    ELSIF DELETING THEN
+        operation_type := 'DELETE';
+    END IF;
+
+    INSERT INTO customers_details_log (
+        customerID,
+        operation_type,
+        old_customer_email,
+        new_customer_email,
+        old_first_name,
+        new_first_name,
+        old_last_name,
+        new_last_name,
+        old_AddressID,
+        new_AddressID
+    )
+    VALUES (
+        :OLD.customerID,
+        operation_type,
+        :OLD.customer_email,
+        :NEW.customer_email,
+        :OLD.first_name,
+        :NEW.first_name,
+        :OLD.last_name,
+        :NEW.last_name,
+        :OLD.AddressID,
+        :NEW.AddressID
+    );
+END;
+/
+CREATE OR REPLACE TRIGGER orders_details_audit
+AFTER INSERT OR UPDATE OR DELETE
+ON orders_details
+FOR EACH ROW
+DECLARE
+    operation_type VARCHAR2(10);
+BEGIN
+    IF INSERTING THEN
+        operation_type := 'INSERT';
+    ELSIF UPDATING THEN
+        operation_type := 'UPDATE';
+    ELSIF DELETING THEN
+        operation_type := 'DELETE';
+    END IF;
+
+    INSERT INTO orders_details_log (
+        orderid,
+        operation_type,
+        old_customerID,
+        new_customerID,
+        old_product_name,
+        new_product_name,
+        old_price,
+        new_price,
+        old_quantity,
+        new_quantity,
+        old_order_date,
+        new_order_date
+    )
+    VALUES (
+        :OLD.orderid,
+        operation_type,
+        :OLD.customerID,
+        :NEW.customerID,
+        :OLD.product_name,
+        :NEW.product_name,
+        :OLD.price,
+        :NEW.price,
+        :OLD.quantity,
+        :NEW.quantity,
+        :OLD.order_date,
+        :NEW.order_date
+    );
+END;
+/
+CREATE OR REPLACE TRIGGER warehouse_inventory_audit
+AFTER INSERT OR UPDATE OR DELETE
+ON warehouse_inventory
+FOR EACH ROW
+DECLARE
+    operation_type VARCHAR2(10);
+BEGIN
+    IF INSERTING THEN
+        operation_type := 'INSERT';
+    ELSIF UPDATING THEN
+        operation_type := 'UPDATE';
+    ELSIF DELETING THEN
+        operation_type := 'DELETE';
+    END IF;
+
+    INSERT INTO warehouse_inventory_log (
+        product_name,
+        warehouse_name,
+        operation_type,
+        old_quantity,
+        new_quantity
+    )
+    VALUES (
+        :OLD.product_name,
+        :OLD.warehouse_name,
+        operation_type,
+        :OLD.quantity,
+        :NEW.quantity
+    );
+END;
+/
+CREATE OR REPLACE TRIGGER product_review_audit
+AFTER INSERT OR UPDATE OR DELETE
+ON product_review
+FOR EACH ROW
+DECLARE
+    operation_type VARCHAR2(10);
+BEGIN
+    IF INSERTING THEN
+        operation_type := 'INSERT';
+    ELSIF UPDATING THEN
+        operation_type := 'UPDATE';
+    ELSIF DELETING THEN
+        operation_type := 'DELETE';
+    END IF;
+
+    INSERT INTO product_review_log (
+        product_name,
+        customerID,
+        operation_type,
+        old_review,
+        new_review,
+        old_review_description,
+        new_review_description,
+        old_review_flag,
+        new_review_flag,
+        timestamp
+    )
+    VALUES (
+        :OLD.product_name,
+        :OLD.customerID,
+        operation_type,
+        :OLD.review,
+        :NEW.review,
+        :OLD.review_description,
+        :NEW.review_description,
+        :OLD.review_flag,
+        :NEW.review_flag,
+        CURRENT_TIMESTAMP
+    );
+END;
+/
+CREATE OR REPLACE TRIGGER warehouse_details_audit
+AFTER INSERT OR UPDATE OR DELETE
+ON warehouse_details
+FOR EACH ROW
+DECLARE
+    operation_type VARCHAR2(10);
+BEGIN
+    IF INSERTING THEN
+        operation_type := 'INSERT';
+    ELSIF UPDATING THEN
+        operation_type := 'UPDATE';
+    ELSIF DELETING THEN
+        operation_type := 'DELETE';
+    END IF;
+
+    INSERT INTO warehouse_details_log (
+        warehouse_name,
+        operation_type,
+        old_AddressID,
+        new_AddressID,
+        timestamp
+    )
+    VALUES (
+        :OLD.warehouse_name,
+        operation_type,
+        :OLD.AddressID,
+        :NEW.AddressID,
+        CURRENT_TIMESTAMP
+    );
+END;
